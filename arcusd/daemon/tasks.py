0f8f815ffff89aacefc858c695f01b7bcc5f32fe
Felipe López <flh.1989@gmail.com>
2018-12-10 12:11:24 -0600

    execute_op function

--- arcusd/daemon/tasks.py
+++ arcusd/daemon/tasks.py
@@ -5,6 +5,7 @@ from ..models.operationinfo import OpInfo
 from ..models.enums.optype import OperationType
 from ..models.enums.opstatus import OperationStatus
 from arcus.client import Client
+from arcus.resources import Transaction
 
 
 ARCUS_API_KEY = os.environ['ARCUS_API_KEY']
@@ -21,68 +22,50 @@ client = Client(ARCUS_API_KEY, ARCUS_SECRET_KEY,
 @app.task
 def topup(biller_id: int, phone_number: str,
           amount: float, currency: str = 'MXN') -> OpInfo:
-    op_info = OpInfo(OperationType.TOPUP)
-    try:
-        topup = client.topups.create(biller_id, phone_number, amount, currency)
-        op_info.status = OperationStatus.SUCCESS
-        op_info.transaction = topup
-    except Exception as exc:
-        op_info.status = OperationStatus.FAILED
-        op_info.error_message = exc.message
-    return op_info
+    return execute_op(OperationType.TOPUP, client.topups.create, biller_id,
+                      phone_number, amount, currency)
 
 
 @app.task
 def query_bill(biller_id: int, account_number: str) -> OpInfo:
-    op_info = OpInfo(OperationType.QUERY)
-    try:
-        bill = client.bills.create(biller_id, account_number)
-        op_info.status = OperationStatus.SUCCESS
-        op_info.operation = bill
-    except Exception as exc:
-        op_info.status = OperationStatus.FAILED
-        op_info.error_message = exc.message
-    return op_info
+    return execute_op(OperationType.QUERY, client.bills.create, biller_id,
+                      account_number)
 
 
 @app.task
 def pay_bill(bill_id: int) -> OpInfo:
-    op_info = OpInfo(OperationType.PAYMENT)
-    try:
+    def pay(bill_id: int) -> Transaction:
         bill = client.bills.get(bill_id)
-        transaction = bill.pay()
-        op_info.transaction = transaction
-        op_info.status = OperationStatus.SUCCESS
-    except Exception as exc:
-        op_info.status = OperationStatus.FAILED
-        op_info.error_message = exc.message
-    return op_info
+        return bill.pay()
+
+    return execute_op(OperationType.PAYMENT, pay, bill_id)
 
 
 @app.task
 def pay_bill(biller_id: int, account_number: str) -> OpInfo:
-    op_info = OpInfo(OperationType.PAYMENT)
-    try:
+    def pay(biller_id: int, account_number: str) -> Transaction:
         bill = client.bills.create(biller_id, account_number)
-        transaction = bill.pay()
-        op_info.operation = transaction
-        op_info.status = OperationStatus.SUCCESS
-    except Exception as exc:
-        op_info.status = OperationStatus.FAILED
-        op_info.error_message = exc.message
-    return op_info
+        return bill.pay()
+
+    return execute_op(OperationType.PAYMENT, pay, biller_id, account_number)
 
 
 @app.task
 def cancell_transaction(transaction_id: int) -> OpInfo:
-    op_info = OpInfo(OperationType.PAYMENT)
-    try:
+    def cancell(transaction_id: int) -> Transaction:
         cancellation = client.transactions.cancel(transaction_id)
-        transaction = client.transactions.get(transaction_id)
+        return client.transactions.get(transaction_id)
+
+    return execute_op(OperationType.PAYMENT, cancell, transaction_id)
+
+
+def execute_op(op_type: OperationType, funct, *args) -> OpInfo:
+    op_info = OpInfo(op_type)
+    try:
+        transaction = funct(*args)
         op_info.operation = transaction
         op_info.status = OperationStatus.SUCCESS
     except Exception as exc:
         op_info.status = OperationStatus.FAILED
         op_info.error_message = exc.message
     return op_info
-
