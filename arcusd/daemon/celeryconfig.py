c21367a22c1aa3c8c6d683a3071ee933589f2b58
Felipe López <flh.1989@gmail.com>
2019-01-14 17:40:06 -0600

    correción update_task_info

--- arcusd/daemon/celeryconfig.py
+++ arcusd/daemon/celeryconfig.py
@@ -4,6 +4,9 @@ broker_url = os.environ['ARCUSD_AMPQ_ADDRESS']
 task_serializer = 'json'
 accept_content = ['json']
 task_default_queue = os.environ['ARCUSD_PAYMENTS_QUEUE']
+imports = [
+    'arcusd.daemon.arcusd_signals',
+]
 include = [
     'arcusd.daemon.tasks',
     'arcusd.daemon.tasks_sync',
