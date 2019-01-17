768a617d485aedb4fa95516ad8987657e62d8eff
Felipe López <flh.1989@gmail.com>
2019-01-17 11:38:13 -0600

    sentry init moved to daemon package

--- arcusd/__init__.py
+++ arcusd/__init__.py
@@ -1,9 +1,1 @@
-import os
-
-import sentry_sdk
-from sentry_sdk.integrations.celery import CeleryIntegration
-
 from .types import OperationType, OperationStatus
-
-sentry_sdk.init(os.environ['ARCUSD_SENTRY_DSN'],
-                integrations=[CeleryIntegration()])
