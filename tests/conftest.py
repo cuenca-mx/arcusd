7745db398e8c989762aa7ecd57738b7d0b5ecb30
Felipe López <flh.1989@gmail.com>
2019-01-17 12:29:55 -0600

    vcr mode new_episodes

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ def vcr_config():
     return dict(
         ignore_hosts={'sentry.io'}
     )
+
+
+@pytest.fixture(scope='module')
+def vcr(vcr):
+    vcr.record_mode = 'new_episodes'
+    return vcr
