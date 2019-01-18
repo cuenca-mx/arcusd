628411d6cc8574ace1c215d23c1f0b7f9e7022f2
Felipe López <37890430+felipaoo@users.noreply.github.com>
2019-01-17 18:34:11 -0600

    default database in connection string, rename arcusd to db (#35)

--- arcusd/data_access/tasks.py
+++ arcusd/data_access/tasks.py
@@ -6,16 +6,16 @@ ARCUSD_DB_MONGO_URL = os.environ['ARCUSD_DB_MONGO_URL']
 
 client = MongoClient(ARCUSD_DB_MONGO_URL)
 
-arcusd = client.arcusd
+db = client.get_database()
 
 
 def save_task_info(task_info: dict):
-    arcusd.tasks.insert_one(task_info)
+    db.tasks.insert_one(task_info)
 
 
 def update_task_info(query: dict, values: dict):
-    arcusd.tasks.find_one_and_update(query, {'$set': values})
+    db.tasks.find_one_and_update(query, {'$set': values})
 
 
 def get_task_info(query):
-    return arcusd.tasks.find_one(query)
+    return db.tasks.find_one(query)
