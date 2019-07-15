d2196cc608957a4ada6b4ed3654ff7003806abb5
JessicaShalomG <jessica@cuenca.com>
2019-07-15 13:16:15 -0500

    changed error message to be added on message when message property not in exc

--- arcusd/daemon/utils.py
+++ arcusd/daemon/utils.py
@@ -19,7 +19,7 @@ def execute_op(request_id: str, op_type: OperationType, funct,
         if hasattr(exc, 'message'):
             op_info.error_message = exc.message
         else:
-            op_info.error_message = 'failed transaction'
+            op_info.error_message = exc.read()
         capture_exception(exc)
     else:
         op_info.operation = transaction
