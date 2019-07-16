dfeecfe5647175efb82ac1935da733462e615c6e
Felipe López <flh.1989@gmail.com>
2019-07-15 21:17:19 -0500

    sentry

--- arcusd/daemon/__init__.py
+++ arcusd/daemon/__init__.py
@@ -3,4 +3,5 @@ import os
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
 
-# sentry_sdk.init('',integrations=[CeleryIntegration()])
+sentry_sdk.init(os.environ['ARCUSD_SENTRY_DSN'],
+                integrations=[CeleryIntegration()])
